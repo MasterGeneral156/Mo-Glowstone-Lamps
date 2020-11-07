c2df2aa2e0f2d60145540368cb8d51aa7569b4cd
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-11-07 17:33:41 -0500

    This is safer.

--- src/main/java/com/themastergeneral/moglowstonelamps/blocks/LampBlock.java
+++ src/main/java/com/themastergeneral/moglowstonelamps/blocks/LampBlock.java
@@ -4,9 +4,12 @@ import com.themastergeneral.ctdcore.block.CTDBlock;
 import com.themastergeneral.moglowstonelamps.MoGlowstoneLamps;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 import net.minecraftforge.common.ToolType;
 
 public class LampBlock extends CTDBlock {
@@ -15,11 +18,16 @@ public class LampBlock extends CTDBlock {
 		super(Block.Properties.create(Material.GLASS, color)
 				.sound(SoundType.LANTERN)
 				.hardnessAndResistance(0.3F)
-				.setLightLevel(s -> 15)
 				.harvestTool(ToolType.PICKAXE)
 				.harvestLevel(1),
 				name, 
 				MoGlowstoneLamps.MODID);
+	}
+	
+	@Override
+	public int getLightValue(BlockState state, IBlockReader world, BlockPos pos)
+	{
+		return 15;
 	}
 
 }
